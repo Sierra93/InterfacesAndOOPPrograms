7a31aaf41d0738050e2815d98667bf38d82bc2e6
Тимошенко Антон Иванович <sierra_93@mail.ru>
2019-03-28 00:04:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleProgramsOOP/Program.cs
@@ -0,0 +1,149 @@
+﻿////Создать абстрактный класс Figure с методами вычисления площади и периметра, а также методом, выводящим информацию о фигуре на экран.
+////Создать производные классы: Rectangle(прямоугольник), Circle(круг), Triangle(треугольник) со своими методами вычисления площади и периметра.
+////Создать массив n фигур и вывести полную информацию о фигурах на экран.
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleProgramsOOP {
+//    class Program {
+//        static void Main(string[] args) {
+//            Rectangle p = new Rectangle();
+//            p.Perimetr();
+//            Rectangle s = new Rectangle();
+//            s.Ploshad();
+//            Console.ReadKey();
+//        }
+//    }
+//    //абстрактный класс фигуры
+//    public abstract class Figure {
+//        public abstract void Perimetr();    //метод вычисляющий периметр 
+//        public abstract void Ploshad(); //метод вычисляющий площадь
+//        public abstract void InfoFigurs();  //метод вывода информации о фигуре 
+//        public int[] infoFigurs = new int[50];
+//    }
+//    //класс прямоугольник
+//    public class Rectangle : Figure {
+//        public override double Perimetr(double a, double b, double c, double p) {
+//            return p = a + b + c;  //вычисление периметра треугольника
+//        }
+//        public override void Ploshad() {
+//            double b = 2, h = 4, s;
+//            s = 1 / 2 * b * h;  //вычисление площади треугольника
+//        }
+//        public override void InfoFigurs() {
+
+//        }
+//    }
+//}
+using System;
+using System.Globalization;
+
+namespace AbstractFigure {
+    internal class Program {
+        private static void Main() {
+            Figure figure1 = new Triangle(4, 5, 6);
+            figure1.ShowInfo();
+
+            Figure figure2 = new Rectangle(5, 6);
+            figure2.ShowInfo();
+
+            Console.ReadLine();
+        }
+        internal abstract class Figure {
+            public abstract string Area();
+            public abstract string Perimeter();
+            public abstract string ShapeName();
+
+            public void ShowInfo() {
+                Console.WriteLine(
+                    $"Название фигуры: {ShapeName()}\n" +
+                    $"Площадь: {Area()}\n" +
+                    $"Периметр: {Perimeter()}"
+                );
+                Console.WriteLine();
+            }
+        }
+        internal class Rectangle : Figure {
+            private double width;   // Ширина прямоугольника
+            private double height;   // Высота прямоугольника
+
+            // Конструктор
+            public Rectangle(double rectangleWidth, double rectangleHeight) {
+                Width = rectangleWidth;
+                Height = rectangleHeight;
+            }
+
+            // Свойство, проверяем значение на отрицательность.
+            public double Width {
+                get => width;
+                set => width = value < 0 ? -value : value;
+            }
+
+            public double Height {
+                get => height;
+                set => height = value < 0 ? -value : value;
+            }
+
+            // Метод для вычисления площади прямоугольника
+            public override string Area() {
+                return (width * height).ToString(CultureInfo.InvariantCulture);
+            }
+
+            // Метод для вычисления периметра прямоугольника
+            public override string Perimeter() {
+                return (width * 2 + height * 2).ToString(CultureInfo.InvariantCulture);
+            }
+
+            // Метод возвращающий наименование фигуры
+            public override string ShapeName() {
+                return "Прямоугольник";
+            }
+        }
+        internal class Triangle : Figure {
+            private double sideA, sideB, sideC;   // Стороны треугольника
+
+            // Конструктор
+            public Triangle(double triangleSideA, double triangleSideB, double triangleSideC) {
+                SideA = triangleSideA;
+                SideB = triangleSideB;
+                SideC = triangleSideC;
+            }
+
+            // Свойство, проверяем значение на отрицательность.
+            // Если значение отрицательное меняем его на аналогичное положительное.
+            public double SideA {
+                get => sideA;
+                set => sideA = value < 0 ? -value : value;
+            }
+
+            public double SideB {
+                get => sideB;
+                set => sideB = value < 0 ? -value : value;
+            }
+
+            public double SideC {
+                get => sideC;
+                set => sideC = value < 0 ? -value : value;
+            }
+
+            // Метод для вычисления площади треугольника
+            public override string Area() {
+                double semPer = (sideA + sideB + sideC) / 2;
+                return Math.Sqrt(semPer * (semPer - sideA) * (semPer - sideB) * (semPer - sideC)).ToString(CultureInfo.InvariantCulture);
+            }
+
+            // Метод для вычисления периметра треугольника
+            public override string Perimeter() {
+                return (sideA + sideB + sideC).ToString(CultureInfo.InvariantCulture);
+            }
+
+            // Метод возвращающий наименование фигуры
+            public override string ShapeName() {
+                return "Треугольник";
+            }
+        }
+    }
+}
---
+++ InterfacesAndOOPPrograms/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndOOPPrograms {
+    //создание двух интерфейсов, описывающих абстрактные методы арифметических операций и операций Sqr и Sqrt
+    public interface IArrOperations {
+        //определяем набор абстрактных методов
+        int Sum();
+        int Otr();
+        int Prz();
+        int Del();
+    }
+    public interface ISqrSqrt {
+        int Sqr(int x);
+        int Sqrt(int x);
+    }
+    //этот класс реализует интерфейс IArOperation
+    class A : IArrOperations {
+        int myX, myY;
+        public int x {
+            set { myX = value; }
+            get { return myX; }
+        }
+        public int y {
+            set { myY = value; }
+            get { return myY; }
+        }
+        public A() { }
+        public A(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        //реализуем методы интерфейса
+        public virtual int Sum() {
+            return x + y;
+        }
+        public int Otr() {
+            return x - y;
+        }
+        public int Prz() {
+            return x * y;
+        }
+        public int Del() {
+            return x / y;
+        }
+        //реализуем собственные методы у этого класса
+        public virtual void Rewrite() {
+            Console.WriteLine("Переменная x: {0} \nПеременная y: {1}", x, y);
+        }
+    }
+    //наследуемся от класса А, и можем переопределить некоторые его методы
+    class Aa : A {
+        public int z;
+        public Aa (int z, int x, int y) : base (x, y) {
+            this.z = z;
+        }
+        //переопределяем метод Sum
+        public override int Sum() {
+            return x + y + z;
+        }
+        public override void Rewrite() {
+            Console.WriteLine("Переменная z: " + z);
+        }
+    }
+    //наследуемся от класса А и реализуем интерфейс ISqrSqrt
+    class Ab : A, ISqrSqrt {
+        public int Sqr(int x) {
+            return x * x;
+        }
+        public int Sqrt(int x) {
+            return (int)Math.Sqrt(x);
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            A obj1 = new A(x: 10, y: 12);
+            Console.WriteLine("obj1: ");
+            obj1.Rewrite();
+            Console.WriteLine("{0} + {1} = {2}", obj1.x, obj1.y, obj1.Sum());
+            Console.WriteLine("{0} * {1} = {2}", obj1.x, obj1.y, obj1.Prz());
+            Aa obj2 = new Aa(z: -3, x: 10, y: 14);
+            Console.WriteLine("\nobj2: ");
+            obj2.Rewrite();
+            Console.WriteLine("{0} + {1} + {3} = {2}", obj2.x, obj2.y, obj2.Sum(), obj2.z);
+            Console.ReadKey();
+        }
+    }
+}
